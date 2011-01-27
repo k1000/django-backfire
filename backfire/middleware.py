f9e9fadc310e0d6a3d06034212b9ddfe331ed00c
kamil <selwak@gmail.com>
2011-01-27 16:01:44 +0000

    corrected js paths

--- backfire/middleware.py
+++ backfire/middleware.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 class BackfireMiddleware(object):
     html = """
-    <script src="%sjs/backfire/backfire-1.0.js"></script>
-	<script src="%sjs/backfire/example.js"></script>
+    <script src="%sjs/backfire/js/backfire-1.0.js"></script>
+	<script src="%sjs/backfire/js/example.js"></script>
 	""" % ( settings.MEDIA_URL, settings.MEDIA_URL )
 	
     def process_response(self, request, response):
